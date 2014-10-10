70172bc2306e16f8f87d29e608c79526714292e0
omgitsvdub <omgitsvdub@gmail.com>
2014-10-10 11:17:22 -0500

    removed some comments and unused code

--- last cleaning bit.rb
+++ last cleaning bit.rb
@@ -6,13 +6,7 @@ t_file = Tempfile.new("another_temp.html")
 # for some reason this wont let me do these in the block with the rest so they are here.
 File.open('jokes_to_be_updated.html', 'r') do |swap|
 	swap.each_line do |line|
-		if line.match(/<P\sCLASS="c9"><SPAN\sCLASS="c2">/)
-			#test = line.sub(/<P\sCLASS="c9"><SPAN\sCLASS="c2">/,'')
-			#t_file.puts test
-		end
-		#test = line.sub(/<\/SPAN><\/P>\n/,'')
 		test = line.gsub(/<P\sCLASS="c9"><SPAN\sCLASS="c2">/," ")
-		#test2 = test2.sub(/<BR><DIV CLASS="c4"><P CLASS="c5"><SPAN CLASS="c7">/,'</P>')
 		t_file.puts test
 	end
 end
